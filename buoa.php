b5764d4cfaee9e356133b8eb2e0f2d37e6cac539
BsNoSi <github@buoa.de>
2018-01-23 18:02:23 +0100

    Initial Release

---
+++ buoa.php
@@ -0,0 +1,12 @@
+<?php
+// Buoa! theme, https://github.com/datenstrom/yellow-themes/tree/master/buoa
+// Copyright (c) 2013-2017 Datenstrom, https://datenstrom.se 
+// This file may be used and distributed under the terms of the public license.
+
+class YellowThemeBuoa
+{
+	const VERSION = "0.7.2";	
+}
+
+$yellow->themes->register("buoa", "YellowThemeBerlin", YellowThemeBuoa::VERSION);
+?>
